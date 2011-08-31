1a230d382d1c9d457ad178ac4e42f8a2444c5065
Oddegamra <oddegamra@gmx.org>
2011-08-31 16:30:29 +0200

    Same for member galleries.

--- images.py
+++ images.py
@@ -51,7 +51,7 @@ def image_from_membergallery(tag):
 	gallery. """
 
 	# Get the actual image URL from the thumbnail URL
-	url = tag[0][0].get("src")
+	url = tag[0][0].get("data-src")
 	thumbnail = __clean_url(url)
 	image_url = thumbnail.replace("_s.", ".")
 
